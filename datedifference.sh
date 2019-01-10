fa7ed237cc5f8a2980b55252ac40896d5548ef87
metalGod <metalaarif@gmail.com>
2019-01-10 14:47:15 +0000

    script to substract dates

--- datedifference.sh
+++ datedifference.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # Substracting different dates
-
+# 2 way user input date difference
 d1=$(date -d "$1" +%s)
 d2=$(date -d "$2" +%s)
 echo $(( (d1 - d2) / 86400 )) days
 
+# only 1 user date difference
+#x=$(date +%d%h)
+#x1=$(date -d "$x" +%s)
+#x2=$(date -d "$1" +%s)
+#echo $(( (x2 - x1) / 86400 )) days
